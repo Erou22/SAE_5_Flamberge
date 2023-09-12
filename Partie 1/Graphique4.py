2c0850dcf6e9d1e1a35bd45ee8f76d5c90cb3e63
Erou22 <56843807+Erou22@users.noreply.github.com>
2023-09-12 14:56:48 +0200

    Update Graphique4.py
    on change tt

--- Partie 1/Graphique4.py
+++ Partie 1/Graphique4.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from Data import data
 
+
 # Supprimer les doublons de films en se basant sur le titre du film
 data_unique = data.drop_duplicates(subset='nomFilm')
 
@@ -14,19 +15,15 @@ filtered_data['genres'] = filtered_data['genres'].str.split(',')
 filtered_data = filtered_data.explode('genres')
 
 # Filtrer les lignes avec '\N' dans la colonne 'note'
-filtered_data = data_unique[data_unique['note'] != '\\N']
+filtered_data = filtered_data[filtered_data['note'] != '\\N']
+filtered_data = filtered_data[filtered_data['genres'] != '\\N']
+
+print(filtered_data)
 
 # Convertir la colonne 'note' en type float
 filtered_data['note'] = filtered_data['note'].astype(float)
 
-
 filtered_data = filtered_data[filtered_data['note'] >= 0]
-
-# Diviser les genres multiples en genres individuels
-filtered_data['genres'] = filtered_data['genres'].str.split(',')
-
-# Utiliser la fonction explode pour diviser les listes de genres en plusieurs lignes
-filtered_data = filtered_data.explode('genres')
 
 # Calculer la moyenne des notes par genre
 moyenne_par_genre = filtered_data.groupby('genres')['note'].mean().reset_index()
