7b4c6e91081f48c9f3634e597d849695b4199dc3
Liebherru <evan22140@gmail.com>
2023-10-04 14:11:00 +0200

    début BDD

---
+++ Partie 2/BDD.sql
@@ -0,0 +1,52 @@
+DROP SCHEMA IF EXISTS flamberge CASCADE;
+CREATE SCHEMA flamberge;
+SET SCHEMA 'flamberge';
+
+CREATE TABLE _films (
+    idFilm SERIAL PRIMARY KEY,
+    titre VARCHAR(100) NOT NULL,
+    anneeSortie INTEGER,
+    noteIMDb FLOAT,
+    nbNote INTEGER
+);
+
+CREATE TABLE _artiste (
+    idArtiste SERIAL PRIMARY KEY,
+    nomArtiste VARCHAR(50)
+);
+  
+CREATE TABLE _metier (
+    idMetier SERIAL PRIMARY KEY,
+    nomMetier VARCHAR(15)
+);
+  
+CREATE TABLE _est_metier (
+    idMetier INTEGER,
+    idArtiste INTEGER,
+    CONSTRAINT metier_fk FOREIGN KEY (idMetier) REFERENCES _metier(idMetier),
+    CONSTRAINT artiste_fk FOREIGN KEY (idArtiste) REFERENCES _artiste(idArtiste),
+  CONSTRAINT est_metier_pk PRIMARY KEY (idMetier,idArtiste)
+);
+  
+CREATE TABLE _role (
+    idFilm INTEGER,
+    idArtiste INTEGER,
+    nomRole VARCHAR(15),
+    CONSTRAINT role_films_fk FOREIGN KEY (idFilm) REFERENCES _films(idFilm),
+    CONSTRAINT role_artiste_fk FOREIGN KEY (idArtiste) REFERENCES _artiste(idArtiste),
+    CONSTRAINT role_pk PRIMARY KEY (idFilm,idArtiste)
+);
+  
+CREATE TABLE _genre (
+    idGenre SERIAL PRIMARY KEY,
+    nomGenre VARCHAR
+);
+  
+CREATE TABLE _possede_genre (
+    idGenre INTEGER,
+    idFilm INTEGER,
+    CONSTRAINT films_fk FOREIGN KEY (idFilm) REFERENCES _films(idFilm),
+    CONSTRAINT genre_fk FOREIGN KEY (idGenre) REFERENCES _genre(idGenre),
+    CONSTRAINT possede_genre_pk PRIMARY KEY (idGenre,idFilm)
+);
+  
