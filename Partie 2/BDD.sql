28ceb192eabfc1c3652f7e6f8eff4a25b446667a
Liebherru <evan22140@gmail.com>
2023-10-10 14:27:46 +0200

    peuplement tables

--- Partie 2/BDD.sql
+++ Partie 2/BDD.sql
@@ -8,7 +8,7 @@ CREATE TABLE _films (
     idFilm SERIAL PRIMARY KEY,
     titre VARCHAR(100) NOT NULL,
     anneeSortie INTEGER,
-    noteIMDb FLOAT,
+    note FLOAT,
     nbVotes INTEGER
 );
 
@@ -19,7 +19,7 @@ CREATE TABLE _artiste (
 
 CREATE TABLE _metier (
     idMetier SERIAL PRIMARY KEY,
-    nomMetier VARCHAR(15)
+    nomMetier VARCHAR(25)
 );
 
 CREATE TABLE _est_metier (
@@ -33,10 +33,10 @@ CREATE TABLE _est_metier (
 CREATE TABLE _role (
     idFilm INTEGER,
     idArtiste INTEGER,
-    nomRole VARCHAR(15),
+    nomRole VARCHAR(25),
     CONSTRAINT role_films_fk FOREIGN KEY (idFilm) REFERENCES _films(idFilm),
     CONSTRAINT role_artiste_fk FOREIGN KEY (idArtiste) REFERENCES _artiste(idArtiste),
-    CONSTRAINT role_pk PRIMARY KEY (idFilm,idArtiste)
+    CONSTRAINT role_pk PRIMARY KEY (idFilm,idArtiste,nomRole)
 );
 
 CREATE TABLE _genre (
@@ -53,15 +53,6 @@ CREATE TABLE _possede_genre (
 );
 -- Fonctions
 
-CREATE OR REPLACE FUNCTION split_genre_f() RETURNS TRIGGER AS 
-$$
-BEGIN
-    INSERT INTO _possede_genre (i)
-        SELECT i FROM SPLIT_PART(idGenre, ',', 1);
-    RETURNS NULL;
-END;
-$$ language plpgsql
-
 -- Peuplement
 
 create table _temp(
@@ -69,27 +60,63 @@ create table _temp(
     metiers VARCHAR(100),
     titre VARCHAR(100),
     anneeSortie INTEGER,
-    noteIMDb FLOAT,
+    note FLOAT,
     nbVotes INTEGER,
     genres VARCHAR(100),
     role varchar(20)
 );
 
-Wbimport -file=../datasetIMDb.csv
-        -table=_temp
-        -delimiter=';'
-        -quoteChar='"'
-        -mode=insert
-        -filecolumns=$wb_skip$,titre,anneeSortie,$wb_skip$,genres,noteIMDb,nbVotes,role,nomArtiste,$wb_skip$,metiers;
+WbimpoRT -FILE=../DATASETIMDB.CSV
+        -TABLE=_TEMP
+        -DELIMITER=';'
+        -QUOTECHAR='"'
+        -MODE=INSERT
+        -filecolumns=$wb_skip$,titre,anneeSortie,$wb_skip$,genres,note,nbVotes,role,nomArtiste,$wb_skip$,metiers;
         
+select * from _temp where titre like '%Semicolon%';
+  
+INSERT INTO _artiste (nomArtiste)
+  SELECT DISTINCT nomArtiste
+  FROM _temp;
+  
+INSERT INTO _films (titre, anneeSortie, note, nbVotes)
+  SELECT DISTINCT titre, anneeSortie, note, nbVotes
+  FROM _temp;
+
+INSERT INTO _metier (nomMetier)
+  SELECT DISTINCT unnest(string_to_array(metiers, ',')) AS nomMetier
+  FROM _temp;
+
+INSERT INTO _est_metier (idArtiste, idMetier)
+  SELECT DISTINCT a.idArtiste, m.idMetier
+  FROM _temp t
+  JOIN _artiste a ON t.nomArtiste = a.nomArtiste
+  JOIN _metier m ON m.nomMetier = ANY(string_to_array(t.metiers, ','));
+
+INSERT INTO _genre (nomGenre)
+  SELECT DISTINCT unnest(string_to_array(genres, ',')) AS nomGenre
+  FROM _temp;
+  
+INSERT INTO _possede_genre (idGenre, idFilm)
+  SELECT DISTINCT g.idGenre, f.idFilm
+  FROM _temp t
+  JOIN _films f ON t.titre = f.titre
+  JOIN _genre g ON g.nomGenre = ANY(string_to_array(t.genres, ','));
+
+
+INSERT INTO _role (idFilm, idArtiste, nomRole)
+  SELECT DISTINCT f.idFilm, a.idArtiste, t.role
+  FROM _temp t
+  JOIN _films f ON t.titre = f.titre
+  JOIN _artiste a ON t.nomArtiste = a.nomArtiste;
+  
+DROP TABLE _temp;
+  
+
 
 -- Triggers
 
-CREATE TRIGGER split_genre_t
-    BEFORE INSERT ON _possede_genre
-    FOR EACH ROW
-    EXECUTE FUNCTION split_genre_f();
+
 
 -- Views
 
-select * from _temp where titre like '%Semicolon%';
