0fd0291c084f2ca0c9f0589738ae4a2e9ea6a81e
Liebherru <evan22140@gmail.com>
2023-11-10 15:32:12 +0100

    plus de clusters

--- Partie 3/Cluster.py
+++ Partie 3/Cluster.py
@@ -22,7 +22,7 @@ data_array = df.values
 cat_columns = [0, 3]  # Assuming 'titre' and 'nomGenre' are categorical columns
 
 # Choose the number of clusters (replace n_clusters with your desired number)
-n_clusters = 3
+n_clusters = 10
 
 # Perform clustering with K-prototype
 kproto = KPrototypes(n_clusters=n_clusters, init='Cao', n_init=5, verbose=2, random_state=42)
