bde4715538abeb1beed4d1999ef123d969cff95e
Liebherru <evan22140@gmail.com>
2023-11-15 08:14:39 +0100

    Genres des films ensemble

--- Partie 3/Cluster.py
+++ Partie 3/Cluster.py
@@ -2,37 +2,56 @@ import Donnees as data
 import pandas as pd
 from kmodes.kprototypes import KPrototypes
 import matplotlib.pyplot as plt
-
-# Chargement des données
-df = data.films_genres
-
 from sklearn.preprocessing import StandardScaler
 
-# Select relevant columns for clustering (excluding 'annee')
-df = data.films_genres[['titre', 'note', 'nbVotes', 'nomGenre']]
+# Chargement des données
 
-# Standardize numerical columns
+dfGenres = data.films_genres.groupby('idFilm')['nomGenre'].agg(list).reset_index()
+
+# Trier les genres par ordre alphabétique
+dfGenres['nomGenre'] = dfGenres['nomGenre'].apply(lambda x: sorted(x))
+
+# Convertir la liste de genres en chaîne de caractères
+dfGenres['nomGenre'] = dfGenres['nomGenre'].apply(lambda x: ','.join(x))
+
+dftout = pd.merge(data.films, dfGenres, on='idFilm')
+
+# Sélectionner les colonnes pertinentes pour le clustering
+df = dftout[['titre', 'note', 'nomGenre']]
+
+# Centrer et réduire les colonnes numériques
 scaler = StandardScaler()
-df[['note', 'nbVotes']] = scaler.fit_transform(df[['note', 'nbVotes']])
+df[['note']] = scaler.fit_transform(df[['note']])
 
-# Convert DataFrame to array for K-prototype
+# Convertir le DataFrame en array
 data_array = df.values
 
-# Specify categorical column indices
-cat_columns = [0, 3]  # Assuming 'titre' and 'nomGenre' are categorical columns
+# Colonnes catégorielles
+cat_columns = [0, 2]
 
-# Choose the number of clusters (replace n_clusters with your desired number)
+# Choisir le nombre de clusters
 n_clusters = 10
 
-# Perform clustering with K-prototype
-kproto = KPrototypes(n_clusters=n_clusters, init='Cao', n_init=5, verbose=2, random_state=42)
+# Effectuer le clustering avec K-prototype
+kproto = KPrototypes(n_clusters=n_clusters, init='Cao', n_init=1, verbose=2, random_state=42)
 clusters = kproto.fit_predict(data_array, categorical=cat_columns)
 
-# Add the cluster labels to the original DataFrame
-data.films_genres['cluster'] = clusters
+# Choosing optimal K
+# cost = []
+# for i in range(2, 10):
+#     kproto = KPrototypes(n_clusters=i, init='Cao', n_init=1, verbose=2, random_state=42)
+#     kproto.fit_predict(data_array, categorical=cat_columns)
+#     cost.append(kproto.cost_)
 
-# Display the resulting DataFrame with cluster labels
-print(data.films_genres[['titre', 'note', 'nbVotes', 'nomGenre', 'cluster']])
+# # Plot the elbow curve
+# plt.plot(range(2, 10), cost, marker='o')
+# plt.xlabel('Number of clusters (K)')
+# plt.ylabel('Inertia (Cost)')
+# plt.title('Elbow Method for Optimal K')
+# plt.show()
 
-# Save the resulting DataFrame to a CSV file
-data.films_genres.to_csv('clusters.csv', index=False)
+# Ajouter les clusters au DataFrame original
+dftout['cluster'] = clusters
+
+# Sauvegarder les clusters dans un fichier CSV
+dftout.to_csv('Partie 3/clusters.csv', index=False)
