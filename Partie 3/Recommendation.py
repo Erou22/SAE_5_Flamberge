c6f154d0020ed09cbc8f70d24cb0f72dcc236194
Erou22 <56843807+Erou22@users.noreply.github.com>
2023-11-15 14:31:22 +0100

    Chuis con

--- Partie 3/Recommendation.py
+++ Partie 3/Recommendation.py
@@ -1,125 +1,47 @@
-from numpy import dot
-from numpy.linalg import norm
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib .pyplot as plt
 
-import math
-import Donnees as data
-import time
+df = pd.read_csv("Partie 3/clusters.csv", delimiter =",")
+print(df)
 
-def transformation_vecteur():
-    vecteur = {}
-    dfGroupeGenre = data.films_genres.groupby("idFilm")["nomGenre"].agg(list).reset_index()
+id_film="1"
 
-    for i , row in dfGroupeGenre.iterrows() :
-        idFilm = row["idFilm"]
-        genres = row["nomGenre"]
-        vecteur[idFilm] = []
-        for j in data.genres["nomGenre"] :  
-            if (j in genres) : 
-                vecteur[idFilm].append(1)
-            else : 
-                vecteur[idFilm].append(0)
-    
-    return vecteur
+df.set_index('idFilm', inplace=True)
 
-def sim_eucli(A,B) :
-    """
-    Similarité sur la distance Euclidienne 
-    """
-    somme = 0 
-    for i in zip(A,B) : 
-        somme += (i[0] - i[1]) * (i[0] - i[1])
-
-    if (somme > 0) : 
-        rep = 1/math.sqrt(somme)
-    else :
-        rep = 1
-    return rep
-
-def sim_cos(A,B) : 
-    """
-    Similarité du cosinus 
-    """
-    return dot(A, B) / (norm(A) * norm(B))
-
-def sim_jaccard(A,B) : 
-    """
-    Similarité : Coefficient de Jaccard 
-    """
-    intersection = sum((a and b) for a, b in zip(A, B))
-    union = sum((a or b) for a, b in zip(A, B))
-
-    # Éviter une division par zéro si les vecteurs sont tous les deux des zéros
-    if union == 0:
-        return 0.0
-
-    # Calculer le coefficient de similarité de Jaccard
-    similarity = intersection / union
-
-    return similarity
-
-# User based
-def prediction_user(A,B) : 
-    somme_sur = 0
-    somme_sous = 0
-    liste_users = list(vecteur_users.keys())
-    liste_users.remove(A)
-    print(liste_users)
-    for i in liste_users :  
-        # print(i,"  ",note[i][B])
-        simi = sim(vecteur_users[A],vecteur_users[i])
-        # print(simi)
-        somme_sur += note[i][B] * simi
-        somme_sous += simi
-    
-    return somme_sur/somme_sous
+# Recherche de film par ID
+def chercher_film_par_id(dataframe, film_id):
+    try:
+        film = dataframe.loc[film_id]
+        return film
+    except KeyError:
+        print(f"Aucun film trouvé avec l'ID {film_id}")
+        return None
 
 
-# Item based
-
-def prediction_item(A,B) : 
-    somme_sur = 0
-    somme_sous = 0
-    liste_items = list(vecteur_items.keys())
-    liste_items.remove(B)
-    print(liste_items)
-    for i in liste_items :  
-        # print(i,"  ",note[A][i])
-        simi = sim(vecteur_items[B],vecteur_items[i])
-        # print(simi)
-        somme_sur += note[A][i] * simi
-        somme_sous += simi
-    
-    return somme_sur/somme_sous
+# Recherche de films par cluster
+def chercher_films_par_cluster(dataframe, cluster):
+    try:
+        films_du_cluster = dataframe[dataframe['cluster'] == int(cluster)]
+        if not films_du_cluster.empty:
+            return films_du_cluster
+        else:
+            print(f"Aucun film trouvé dans le cluster {cluster}")
+            return None
+    except KeyError:
+        print(f"Aucun film trouvé dans le cluster {cluster}")
+        return None
 
 
-# Main programme
-vecteurs = transformation_vecteur()
-nom_film = "1"
+# Programme principal
+while True:
+    id_film = input("Sur quel film voulez-vous faire la recommandation (id) ? (Entrez -1 pour quitter)\n")
 
-while nom_film != "-1" : 
-    nom_film = input("Sur quel film voulez vous faire la recommandation (-1 pour stop) ?\n")
+    if id_film == "-1":
+        break
 
-    if nom_film != "-1" :
-        start = time.time()     # chrono
+    film_trouve = chercher_film_par_id(df, int(id_film))
 
-        recommandation = {}
-        id_vecteur = data.films_genres.loc[data.films_genres['titre'] == nom_film, 'idFilm'].values[0]   # Donne l'ID du film selon son titre
-        for i in list(vecteurs.keys()) :
-            recommandation[i] = sim_jaccard(vecteurs[id_vecteur], vecteurs[i])   # Similarité entre les différents vecteurs 
-
-        recommandation_trie = dict(sorted(recommandation.items(), key=lambda item: item[1], reverse = True))  # Trie pour avoir les plus haut taux de similarité en premier
-        recommandation_trie_test = dict(list(recommandation_trie.items())[:10])     # Sélectionne les 10 premiers éléments pour faire la reommandation 
-
-        # Montre les films 
-        print("------------- Résultats ---------------")
-        for i in recommandation_trie_test.items() : 
-            print(data.films_genres.loc[data.films_genres['idFilm'] == i[0], 'titre'].values[0],
-            "\n ID film : ", i[0],
-            "\n Genre : ", data.films_genres.loc[data.films_genres['idFilm'] == i[0], 'nomGenre'].values,
-            "\n Taux similarité : ", float(i[1]*100)," %\n")
-
-        stop = time.time() - start
-
-        print("------------- Stats ---------------\nTemps d'execution : ",stop,"\n")
-        # print(recommandation)
-print("slt :(")
+    if film_trouve is not None:
+        df_filtre = chercher_films_par_cluster(df,film_trouve["cluster"])
+        print(df_filtre)
