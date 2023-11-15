b6014ee6d936c660c29ad19145950051d9d732d3
Erou22 <56843807+Erou22@users.noreply.github.com>
2023-11-15 16:35:20 +0100

    Ok tier

--- Partie 3/IA_vecteur.py
+++ Partie 3/IA_vecteur.py
@@ -1,5 +1,6 @@
 from numpy import dot
 from numpy.linalg import norm
+from numpy import random
 
 import math
 import Donnees as data
@@ -91,35 +92,74 @@ def prediction_item(A,B) :
     
     return somme_sur/somme_sous
 
+def recommandation(nom_film) :
+    start = time.time()     # chrono
+
+    recommandation = {}
+    id_vecteur = data.films_genres.loc[data.films_genres['titre'] == nom_film, 'idFilm'].values[0]   # Donne l'ID du film selon son titreS
+    for i in list(vecteurs.keys()) :
+        recommandation[i] = sim_eucli(vecteurs[id_vecteur], vecteurs[i])   # Similarité entre les différents vecteurs 
+
+    # pour ne pas recommandé le même film
+    del recommandation[id_vecteur]
+
+    recommandation_trie = dict(sorted(recommandation.items(), key=lambda item: item[1], reverse = True))  # Trie pour avoir les plus haut taux de similarité en premier
+    recommandation_trie_premiers = dict(list(recommandation_trie.items())[:10])     # Sélectionne les 10 premiers éléments pour faire la recommandation 
+    
+    max_value = max(recommandation_trie.values())
+    max_values = [key for key, value in recommandation_trie.items() if value == max_value]
+
+    if len(max_values) > 10 : 
+        # Ici on prends les films qui ont la note maximal sur la similarité
+        # donc sont égaux, pour ne pas faire resortir les mêmes films en boucle 
+        reco_random_top = random.choice(max_values, 10)
+
+        # Montre les films 
+        print("------------- Résultats ---------------")
+        for i in reco_random_top : 
+            print(data.films_genres.loc[data.films_genres['idFilm'] == i, 'titre'].values[0],
+            "\n ID film : ", i,
+            "\n Genre : ", data.films_genres.loc[data.films_genres['idFilm'] == i, 'nomGenre'].values,
+            "\n Taux similarité : ", float(recommandation[i]*100)," %\n")
+
+
+    else : 
+        # Montre les films 
+        print("------------- Résultats ---------------")
+        for i in recommandation_trie_premiers.items() : 
+            print(data.films_genres.loc[data.films_genres['idFilm'] == i[0], 'titre'].values[0],
+            "\n ID film : ", i[0],
+            "\n Genre : ", data.films_genres.loc[data.films_genres['idFilm'] == i[0], 'nomGenre'].values,
+            "\n Taux similarité : ", float(i[1]*100)," %\n")
+
+
+    # Enlever pour avoir les premiers résultat
+    # print("------------- Résultats ---------------")
+    # for i in recommandation_trie_premiers.items() : 
+    #     print(data.films_genres.loc[data.films_genres['idFilm'] == i[0], 'titre'].values[0],
+    #     "\n ID film : ", i[0],
+    #     "\n Genre : ", data.films_genres.loc[data.films_genres['idFilm'] == i[0], 'nomGenre'].values,
+    #     "\n Taux similarité : ", float(i[1]*100)," %\n")
+
+    stop = time.time() - start
+
+    print("------------- Stats ---------------\nTemps d'execution : ",stop,"\n")
+    # print(recommandation)
 
 # Main programme
 vecteurs = transformation_vecteur()
 nom_film = "1"
 
 while nom_film != "-1" : 
-    nom_film = input("Sur quel film voulez vous faire la recommandation (-1 pour stop) ?\n")
+    nom_film = input("Sur quel film voulez vous faire la recommandation (nom du film sans faute ni d'espace après) ? (Entrez -1 pour quitter)\n")
 
     if nom_film != "-1" :
-        start = time.time()     # chrono
-
-        recommandation = {}
-        id_vecteur = data.films_genres.loc[data.films_genres['titre'] == nom_film, 'idFilm'].values[0]   # Donne l'ID du film selon son titre
-        for i in list(vecteurs.keys()) :
-            recommandation[i] = sim_jaccard(vecteurs[id_vecteur], vecteurs[i])   # Similarité entre les différents vecteurs 
-
-        recommandation_trie = dict(sorted(recommandation.items(), key=lambda item: item[1], reverse = True))  # Trie pour avoir les plus haut taux de similarité en premier
-        recommandation_trie_test = dict(list(recommandation_trie.items())[:10])     # Sélectionne les 10 premiers éléments pour faire la reommandation 
-
-        # Montre les films 
-        print("------------- Résultats ---------------")
-        for i in recommandation_trie_test.items() : 
-            print(data.films_genres.loc[data.films_genres['idFilm'] == i[0], 'titre'].values[0],
-            "\n ID film : ", i[0],
-            "\n Genre : ", data.films_genres.loc[data.films_genres['idFilm'] == i[0], 'nomGenre'].values,
-            "\n Taux similarité : ", float(i[1]*100)," %\n")
-
-        stop = time.time() - start
-
-        print("------------- Stats ---------------\nTemps d'execution : ",stop,"\n")
-        # print(recommandation)
-print("slt :(")
+        recommandation(nom_film)
+    
+    else : 
+        rep = input("C'est un film ? (y/n) ?\n")
+        if (rep == "y") : 
+            print("Ce n'est pas bien de mentir, il n'y a pas de films \"-1\" dans cette BDD, il y aura donc une erreur...")
+            recommandation(nom_film)
+        else :
+            print("Au revoir :(")
