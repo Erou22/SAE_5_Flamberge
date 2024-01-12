3850523f32fe14de21d2e340effabdb5cc036147
Erou22 <erwan.fertray@gmail.com>
2024-01-12 12:24:33 +0100

    detail film + footer

--- Partie_5/script.js
+++ Partie_5/script.js
@@ -1,4 +1,35 @@
-// Déployer la section au clic sur le titre
+// Afficher le menu burger
+function toggleMenu() {
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('show');
+}
+
+/* ----------------------- */
+/*  Bouton retour en haut  */
+/* ----------------------- */
+
+window.onscroll = function () {
+    afficherBouton();
+};
+
+function afficherBouton() {
+    var boutonRetourHaut = document.getElementById("retourHaut");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        boutonRetourHaut.style.display = "block";
+    } else {
+        boutonRetourHaut.style.display = "none";
+    }
+}
+
+// Fonction pour retourner en haut de la page
+function retourEnHaut() {
+    document.body.scrollTop = 0; // Pour les navigateurs Safari
+    document.documentElement.scrollTop = 0; // Pour les autres navigateurs
+}
+
+/* -------------------------------- */
+/*  Documentation - Menu déroulant  */
+/* -------------------------------- */
 document.addEventListener("DOMContentLoaded", function () {
     const clickableSections = document.querySelectorAll(".clickable-section");
 
