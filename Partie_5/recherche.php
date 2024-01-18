c70a6465034fa8861a28ff23d74239f7d52556ca
ALLAIN MATHEO <maallain@mx21>
2024-01-18 10:27:27 +0100

    rien

--- Partie_5/recherche.php
+++ Partie_5/recherche.php
@@ -2,7 +2,6 @@
 <html lang="fr">
 
 <head>
-  <?php require("./film.php") ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Recherche</title>
